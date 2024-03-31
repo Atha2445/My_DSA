95ef2b864a8584dec837c1f479b65ba197509585
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2024-03-31 10:10:12 +0530

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int> s;
+    int n,ele;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>ele;
+        s.push(ele);
+    }
+    while(!s.empty()){
+        cout<<s.bottom()<<" ";
+        s.pop();
+    }
+    return 0;
+}
